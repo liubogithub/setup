6eae7e425bedc99c862e48db0dfcf19c87d2357a
Liu Bo <bo.li.liu@oracle.com>
2016-04-12 19:13:13 -0700

    sysbench: more filesystem options and add block size for random and sequential
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>

--- perf_sysbench.sh
+++ perf_sysbench.sh
@@ -12,17 +12,31 @@ M=/mnt/btrfs
 D=/dev/sdf
 
 echo "prepare..."
+ulimit -n 10240
 umount $M $D
 
-mkfs.btrfs -f $D || exit
+if [ "x$1" = "xbtrfs" ]; then
+	mkfs.btrfs -f $D || exit
+elif [ "x$1" = "xxfs" ]; then
+	mkfs.xfs -f $D || exit
+elif [ "x$1" = "xext4" ]; then
+	mkfs.ext4 -F -q $D || exit
+fi
+
 mount $D $M || exit
 
 echo "perf start..."
 cd $M
-id=$((RANDOM % 1000))
+id="`date +%d%m%y_%H%M%S`"
 
 $tsb --num-threads=16 --test=fileio --file-total-size=4G --file-test-mode=rndrw prepare
-$tsb --num-threads=16 --test=fileio --file-block-size=4K --file-total-size=4G --file-test-mode=rndrw run > /tmp/perf_sysbench_${id}.log 2>&1
+
+# random rw use 8K to match DB workload
+$tsb --num-threads=16 --test=fileio --file-block-size=8K --file-total-size=4G --file-test-mode=rndrw run > /tmp/perf_sysbench_${1}_${id}.log 2>&1
+
+# sequential rw use 1M to match DB workload
+#$tsb --num-threads=16 --test=fileio --file-block-size=8K --file-total-size=4G --file-test-mode=rndrw run > /tmp/perf_sysbench_${id}.log 2>&1
+
 $tsb --num-threads=16 --test=fileio --file-total-size=4G --file-test-mode=rndrw cleanup
 cd -
-echo "check /tmp/perf_sysbench_${id}.log..."
+echo "check /tmp/perf_sysbench_${1}_${id}.log..."
